0d80e70ec2c268c1a4db0f69ed55005e397645af
Margaux <cortier.margaux@gmail.com>
2018-11-05 18:51:56 +0000

    rename password name

--- PrivateClub/db/schema.rb
+++ PrivateClub/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_145941) do
+ActiveRecord::Schema.define(version: 2018_11_05_184912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_145941) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
